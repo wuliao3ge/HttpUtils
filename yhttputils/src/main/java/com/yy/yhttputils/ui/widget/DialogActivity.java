b1b0438fb760ddc39ebdfce50eb1e07182b49403
wuliao3ge <1046778620@qq.com>
2018-06-13 17:33:53 +0800

    漏传

---
+++ yhttputils/src/main/java/com/yy/yhttputils/ui/widget/DialogActivity.java
@@ -0,0 +1,16 @@
+package com.yy.yhttputils.ui.widget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yy.yhttputils.R;
+
+
+public class DialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+    }
+}
